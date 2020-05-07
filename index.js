f476c5c3d8d6925124decb5f738aef742d1d0786
Javier Umana <javierumanafs@gmail.com>
2020-05-07 13:14:35 -0400

    hello

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
 const server = require('./server.js');
 
-server.listen(5000, () => {
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
   console.log(' *Server running on port 5000* ');
 });
